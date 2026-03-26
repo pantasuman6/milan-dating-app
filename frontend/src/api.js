9bc2374a0f5b0503360297e0d6b248c3338ddbaf
pantasuman6 <pantasuman6@gmail.com>
2026-03-25 22:57:46 -0400

    Point frontend to Railway backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://milan-dating-app-production.up.railway.app',
 });
 
 api.interceptors.request.use((config) => {
